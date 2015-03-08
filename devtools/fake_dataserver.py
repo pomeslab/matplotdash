ee31dcd69877fe5690eeb81508d30734bfb4a48e
Christopher Ing <ing.chris@gmail.com>
2015-03-07 23:26:22 -0500

    hardcoded variable to reduce points per hour

--- devtools/fake_dataserver.py
+++ devtools/fake_dataserver.py
@@ -6,28 +6,27 @@ for live updating plot rendering in the browser. The test datafile has
 Examples
 --------
 >>> python fake_dataserver.py
->>> import requests; requests.get('http://127.0.0.1:5000/10').content
+>>> import requests; requests.get('http://127.0.0.1:5000/ts/10').content
 
 """
 
 from datetime import datetime
 from flask import Flask, jsonify
 app = Flask(__name__)
+points_per_hour = 6000
 
 # Open local datafile (omit the standard error column)
 with open ("time_val_error_data.dat","r") as datafile:
-    lines = datafile.readlines()
+    lines = datafile.readlines()[:points_per_hour]
     (t, v) = zip(*[(float(l.split()[0]), float(l.split()[1])) for l in lines])
 
-@app.route("/", defaults={'points': 100})
-@app.route("/<int:points>", methods=["GET"])
+@app.route("/ts", defaults={'points': 100})
+@app.route("/ts/<int:points>", methods=["GET"])
 def get_data(points):
     now = datetime.now()
     fraction=int(len(t)*(now.minute+(now.second/60.0))/60.0)
-    t_slice = t[fraction:fraction+points]
-    v_slice = v[fraction:fraction+points]
-    ts = {'time': [t_slice],
-          'value': [v_slice]}
+    ts = {'time': t[fraction:fraction+points],
+          'value': v[fraction:fraction+points]}
     return jsonify({'ts': ts})
 
 if __name__ == "__main__":
